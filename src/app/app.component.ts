3de2db70fe674d83dbb176e8e2e3552e7eedf45c
jastikka-03 <jastikka.p03@gmail.com>
2024-08-02 06:49:31 +0530

    first commit-without connect api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'AngularCrud';
