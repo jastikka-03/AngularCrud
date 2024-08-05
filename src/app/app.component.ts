c33cb35971b31899aab9eddfe82e5c91094b2562
jastikka-03 <jastikka.p03@gmail.com>
2024-08-05 06:38:23 +0530

    connected with api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeService } from './service/employee.service';
+import { Employee } from './models/employee';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  Employeeary : Employee[] = [];
+
+  Employeeformgroup :FormGroup;
+
+  constructor(private empservice: EmployeeService, private fb:FormBuilder){
+    this.Employeeformgroup =this.fb.group({
+      id:[""],
+      name:[""],
+      mobileNo:[""],
+      emailID:[""]
+    })
+  }
+
+  ngOnInit(): void {
+    this.getemployeees();
+  }
+
+  getemployeees(){
+    this.empservice.GetEmployee().subscribe(
+      response=>{
+        console.log(response);
+        this.Employeeary = response;
+      }
+    )
+  }
+
+  onsubmit(){
+    console.log(this.Employeeformgroup.value);
+    if(this.Employeeformgroup.value.id != null && this.Employeeformgroup.value.id !=""){
+      this.empservice.UpdateEmployee(this.Employeeformgroup.value).subscribe(response=>{
+        console.log(response);
+        this.getemployeees();
+        this.Employeeformgroup.setValue({
+          id:"",
+          name:"",
+          mobileNo:"",
+          emailID:"",
+        })
+      })
+    }
+    else{
+      this.empservice.CreateEmployee(this.Employeeformgroup.value).subscribe(response=>{
+        console.log(response);
+        this.getemployeees();
+        this.Employeeformgroup.setValue({
+          id:"",
+          name:"",
+          mobileNo:"",
+          emailID:"",
+        })
+      })
+    }
+  }
+
+  Fillform(emp:Employee){
+    this.Employeeformgroup.setValue({
+      id:emp.id,
+      name:emp.name,
+      mobileNo:emp.mobileNo,
+      emailID:emp.emailID,
+    })
+  }
+  
+  DeleteEmp(id: string){
+    this.empservice.DeleteEmployee(id).subscribe(res=>{
+      console.log(res);
+      this.getemployeees();
+    })
+  }
   title = 'AngularCrud';
 }
--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  constructor(private httpclient:HttpClient) { }
+
+  baseurl="http://localhost:5086/api/Employee";
+
+  GetEmployee(): Observable<Employee[]>{
+    return this.httpclient.get<Employee[]>(this.baseurl)
+  }
+
+  CreateEmployee(emp : Employee):Observable<Employee>{
+    emp.id="00000000-0000-0000-0000-000000000000";
+    return this.httpclient.post<Employee>(this.baseurl,emp)
+  }
+
+  UpdateEmployee(emp: Employee) : Observable<Employee> {
+    return this.httpclient.put<Employee>(this.baseurl +'/'+emp.id,emp);
+  }
+
+  DeleteEmployee(id: string) : Observable<Employee> {
+    return this.httpclient.delete<Employee>(this.baseurl +'/'+id);
+  }
 }
